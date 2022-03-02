b07e61cd93d79ad16367d9a349dfbeb8fea91e56
rsudo <badbad7351@gmail.com>
2022-03-02 15:50:52 +0900

    first commit

---
+++ app/handler/accounts/create.go
@@ -0,0 +1,45 @@
+package accounts
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"yatter-backend-go/app/domain/object"
+	"yatter-backend-go/app/handler/httperror"
+)
+
+// Request body for `POST /v1/accounts`
+type AddRequest struct {
+	Username string
+	Password string
+}
+
+// Handle request for `POST /v1/accounts`
+func (h *handler) Create(w http.ResponseWriter, r *http.Request) {
+	// ctx := r.Context()
+
+	var req AddRequest
+	d := json.NewDecoder(r.Body)
+	if err := d.Decode(&req); err != nil {
+		httperror.BadRequest(w, err)
+		return
+	}
+
+	account := new(object.Account)
+	account.Username = req.Username
+	if err := account.SetPassword(req.Password); err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+
+	// domain/repository の取得
+	// dbにマッピングするために使う？
+	_ = h.app.Dao.Account()
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(account); err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+}
