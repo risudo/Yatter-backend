dbc30109fd032b2e98ce7f12398829108ba5c0d1
rsudo <badbad7351@gmail.com>
2022-03-08 21:59:52 +0900

    use func request.IDOf

--- app/handler/status/get.go
+++ app/handler/status/get.go
@@ -3,16 +3,14 @@ package status
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 	"yatter-backend-go/app/handler/httperror"
-
-	"github.com/go-chi/chi"
+	"yatter-backend-go/app/handler/request"
 )
 
 func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	repo := h.app.Dao.Status()
-	id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
+	id, err := request.IDOf(r)
 	if err != nil {
 		httperror.InternalServerError(w, err)
 		return
@@ -28,6 +26,7 @@ func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
 		httperror.Error(w, 404)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(status); err != nil {
 		httperror.InternalServerError(w, err)
