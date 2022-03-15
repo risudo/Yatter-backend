db580565599c0adb7355d986ae08f345c4d1f825
rsudo <badbad7351@gmail.com>
2022-03-15 17:41:16 +0900

    implement "GET /v1/accounts/username/followers"

--- app/domain/repository/relation.go
+++ app/domain/repository/relation.go
@@ -8,4 +8,5 @@ import (
 type Relation interface {
 	Follow(ctx context.Context, followingID object.AccountID, followerID object.AccountID) error
 	Following(ctx context.Context, followingID object.AccountID) ([]object.Account, error)
+	Followers(ctx context.Context, followerID object.AccountID) ([]object.Account, error)
 }
