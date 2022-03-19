e5cd4892d0439b96abc8e485c2c137c3f367f285
rsudo <badbad7351@gmail.com>
2022-03-20 08:27:30 +0900

    implement "GET /v1/timelines/home"

---
+++ app/handler/timelines/home.go
@@ -0,0 +1,28 @@
+package timelines
+
+import (
+	"encoding/json"
+	"net/http"
+	"yatter-backend-go/app/handler/auth"
+	"yatter-backend-go/app/handler/httperror"
+)
+
+func (h *handler) Home(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	login := auth.AccountOf(r)
+	if login == nil {
+		return
+	}
+
+	timeline, err := h.app.Dao.Status().HomeTimeline(ctx, login.ID)
+	if err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(timeline); err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+}
