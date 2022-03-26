44b590d4dd45151f4862f481d6f00d401e3cb7db
rsudo <badbad7351@gmail.com>
2022-03-26 16:46:14 +0900

    change signature account repository "Create"

--- app/handler/accounts/create.go
+++ app/handler/accounts/create.go
@@ -44,13 +44,14 @@ func (h *handler) Create(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// データベースにアカウント作成
-	if err := accountRepo.Create(ctx, account); err != nil {
+	entity, err := accountRepo.Create(ctx, account)
+	if err != nil {
 		httperror.InternalServerError(w, err)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(account); err != nil {
+	if err := json.NewEncoder(w).Encode(entity); err != nil {
 		httperror.InternalServerError(w, err)
 		return
 	}
