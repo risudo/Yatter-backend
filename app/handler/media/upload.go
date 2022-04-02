d506c608a8619f52da0f72c289c82ce1e43bbbdd
rsudo <badbad7351@gmail.com>
2022-04-02 14:42:13 +0900

    wip upload image

--- app/handler/media/upload.go
+++ app/handler/media/upload.go
@@ -1,6 +1,36 @@
 package media
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"yatter-backend-go/app/handler/httperror"
+)
 
 func (h *handler) Upload(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(32 << 20)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fileSrc, _, err := r.FormFile("file")
+	if err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+	defer fileSrc.Close()
+	fileDest, err := os.Create("tmp.png")
+	if err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+	defer fileDest.Close()
+
+	io.Copy(fileDest, fileSrc)
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(&struct{}{}); err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
 }
