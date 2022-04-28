95911871828ca3374a1d4f3d467a37b66b1598b2
rsudo <badbad7351@gmail.com>
2022-04-28 15:37:49 +0900

    add testcase for parameters

--- app/handler/parameters/parameters_test.go
+++ app/handler/parameters/parameters_test.go
@@ -1,15 +1,18 @@
 package parameters_test
 
 import (
+	"math"
 	"net/http"
 	"testing"
+	"yatter-backend-go/app/domain/object"
 	"yatter-backend-go/app/handler/handler_test_setup"
 	"yatter-backend-go/app/handler/parameters"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParse(t *testing.T) {
+func TestParseErr(t *testing.T) {
 	m := handler_test_setup.MockSetup()
 	defer m.Close()
 
@@ -63,12 +66,142 @@ func TestParse(t *testing.T) {
 			}(),
 			expect_is_err: true,
 		},
+		{
+			name: "OverMaxLimit",
+			actual_is_err: func() bool {
+				req, err := http.NewRequest("GET", m.AsURL("/v1/timelines/public"), nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				params := req.URL.Query()
+				params.Add("limit", "81")
+				req.URL.RawQuery = params.Encode()
+				_, err = parameters.ParseAll(req)
+				return err != nil
+			}(),
+			expect_is_err: true,
+		},
+		{
+			name: "LessMinLimit",
+			actual_is_err: func() bool {
+				req, err := http.NewRequest("GET", m.AsURL("/v1/timelines/public"), nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				params := req.URL.Query()
+				params.Add("limit", "-1")
+				req.URL.RawQuery = params.Encode()
+				_, err = parameters.ParseAll(req)
+				return err != nil
+			}(),
+			expect_is_err: true,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if !assert.Equal(t, tt.expect_is_err, tt.actual_is_err) {
 				return
+			}
+		})
+	}
+}
+
+func TestParseAll(t *testing.T) {
+	m := handler_test_setup.MockSetup()
+	defer m.Close()
+
+	tests := []struct {
+		name   string
+		actual *object.Parameters
+		expect *object.Parameters
+	}{
+		{
+			name: "only_media",
+			actual: func() *object.Parameters {
+				req, err := http.NewRequest("GET", m.AsURL("/v1/timelines/public"), nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				params := req.URL.Query()
+				params.Add("only_media", "1")
+				req.URL.RawQuery = params.Encode()
+				param, err := parameters.ParseAll(req)
+				return param
+			}(),
+			expect: &object.Parameters{
+				OnlyMedia: true,
+				MaxID:     math.MaxInt64,
+				SinceID:   0,
+				Limit:     parameters.DefaultLimit,
+			},
+		},
+		{
+			name: "max_id",
+			actual: func() *object.Parameters {
+				req, err := http.NewRequest("GET", m.AsURL("/v1/timelines/public"), nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				params := req.URL.Query()
+				params.Add("max_id", "90")
+				req.URL.RawQuery = params.Encode()
+				param, err := parameters.ParseAll(req)
+				return param
+			}(),
+			expect: &object.Parameters{
+				OnlyMedia: false,
+				MaxID:     90,
+				SinceID:   0,
+				Limit:     parameters.DefaultLimit,
+			},
+		},
+		{
+			name: "since_id",
+			actual: func() *object.Parameters {
+				req, err := http.NewRequest("GET", m.AsURL("/v1/timelines/public"), nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				params := req.URL.Query()
+				params.Add("since_id", "10")
+				req.URL.RawQuery = params.Encode()
+				param, err := parameters.ParseAll(req)
+				return param
+			}(),
+			expect: &object.Parameters{
+				OnlyMedia: false,
+				MaxID:     math.MaxInt64,
+				SinceID:   10,
+				Limit:     parameters.DefaultLimit,
+			},
+		},
+		{
+			name: "limit",
+			actual: func() *object.Parameters {
+				req, err := http.NewRequest("GET", m.AsURL("/v1/timelines/public"), nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				params := req.URL.Query()
+				params.Add("limit", "10")
+				req.URL.RawQuery = params.Encode()
+				param, err := parameters.ParseAll(req)
+				return param
+			}(),
+			expect: &object.Parameters{
+				OnlyMedia: false,
+				MaxID:     math.MaxInt64,
+				SinceID:   0,
+				Limit:     10,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if d := cmp.Diff(tt.actual, tt.expect); len(d) != 0 {
+				t.Fatalf("differs: (-got +want)\n%s", d)
 			}
 		})
 	}
