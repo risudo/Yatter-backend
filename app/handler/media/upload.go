f56e24c14ed17ea6e00fd1be9067189dba45dc4e
rsudo <badbad7351@gmail.com>
2022-05-07 10:58:27 +0900

    update upload

--- app/handler/media/upload.go
+++ app/handler/media/upload.go
@@ -3,6 +3,7 @@ package media
 import (
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -16,35 +17,36 @@ func mediatype(contentType string) string {
 		return "image"
 	} else if strings.Contains(contentType, "video") {
 		return "video"
+	} else if strings.Contains(contentType, "gifv") {
+		return "gifv"
 	}
 	return "unknown"
 }
 
+// Handle request for "POST /v1/media"
 func (h *handler) Upload(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	description := r.FormValue("description")
 
-	fileSrc, header, err := r.FormFile("file")
+	src, header, err := r.FormFile("file")
 	if err != nil {
 		httperror.InternalServerError(w, err)
 		return
 	}
-	defer fileSrc.Close()
-
-	mediatype := mediatype(header.Header["Content-Type"][0])
-	url := files.CreateURL(header.Filename)
+	defer func() {
+		if err := src.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
 
 	attachment := &object.Attachment{
-		MediaType:   mediatype,
-		URL:         url,
+		MediaType:   mediatype(header.Header["Content-Type"][0]),
+		URL:         files.CreateURL(header.Filename),
 		Description: &description,
 	}
-
-	attachment.ID, err = h.app.Dao.Attachment().Insert(ctx, *attachment)
-	if err != nil {
-		httperror.InternalServerError(w, err)
-		return
+	if *attachment.Description == "" {
+		attachment.Description = nil
 	}
 
 	err = files.MightCreateAttachmentDir()
@@ -53,14 +55,28 @@ func (h *handler) Upload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fileDest, err := os.Create(attachment.URL)
+	dest, err := os.Create(attachment.URL)
 	if err != nil {
 		httperror.InternalServerError(w, err)
 		return
 	}
-	defer fileDest.Close()
+	defer func() {
+		if err := dest.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
 
-	io.Copy(fileDest, fileSrc)
+	_, err = io.Copy(dest, src)
+	if err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+
+	attachment.ID, err = h.app.Dao.Attachment().Insert(ctx, *attachment)
+	if err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(attachment); err != nil {
