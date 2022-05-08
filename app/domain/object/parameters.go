3969606185f8caeee4c066989230d21ef5bff017
rsudo <badbad7351@gmail.com>
2022-05-08 10:36:40 +0900

    add comment object/parameters

--- app/domain/object/parameters.go
+++ app/domain/object/parameters.go
@@ -1,8 +1,15 @@
 package object
 
 type Parameters struct {
+	// Only return statuses that have media attachments (public and tag timelines only)
 	OnlyMedia bool
+
+	// Get a list of followings with ID less than this value
 	MaxID     int64
+
+	// Get a list of followings with ID greater than this value
 	SinceID   int64
+
+	// Maximum number of followings to get (Default 40, Max 80)
 	Limit     int
 }
