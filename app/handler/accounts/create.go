2ad25bcd0ccd67286404ef38b4dcb7c700ad9ac3
rsudo <badbad7351@gmail.com>
2022-05-13 21:58:38 +0900

    refactor accounts/create

--- app/handler/accounts/create.go
+++ app/handler/accounts/create.go
@@ -1,52 +1,101 @@
 package accounts
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"yatter-backend-go/app/domain/object"
+	"yatter-backend-go/app/domain/repository"
 	"yatter-backend-go/app/handler/httperror"
 )
 
 // Request body for "POST /v1/accounts"
 type AddRequest struct {
-	Username string
-	Password string
+	Username     string
+	Password     string
+	Display_Name string
+	Note         string
+	Avatar       string
+	Header       string
+}
+
+// custom error type
+type (
+	errBadrequest struct {
+		message string
+	}
+
+	errConflict struct{}
+)
+
+func (e *errBadrequest) Error() string {
+	return e.message
+}
+
+func (e *errConflict) Error() string {
+	return ""
+}
+
+/*
+Example Request body
+
+{
+   "username":"john",
+   "password":"P@ssw0rd",
+   "avatar":"attachments/2022-05-13T02:14:24.4652768Z.png",
+   "note":"note",
+   "header":"attachments/2022-05-13T02:14:24.4652768Z.png",
+   "display_name":"display "
+}
+*/
+
+func parseRequest(ctx context.Context, r *http.Request, repo repository.Account) (*object.Account, error) {
+	var req AddRequest
+	d := json.NewDecoder(r.Body)
+	if err := d.Decode(&req); err != nil {
+		return nil, &errBadrequest{message: err.Error()}
+	}
+
+	if len(req.Username) < 1 {
+		return nil, &errBadrequest{message: "empty username"}
+	}
+
+	// 同じユーザー名がいるかチェック
+	a, err := repo.FindByUsername(ctx, req.Username)
+	if err != nil {
+		return nil, err
+	} else if a != nil {
+		return nil, &errConflict{}
+	}
+
+	account := &object.Account{
+		Username: req.Username,
+	}
+
+	if err = account.SetPassword(req.Password); err != nil {
+		return nil, err
+	}
+	return account, nil
 }
 
 // Handle request for "POST /v1/accounts"
 func (h *handler) Create(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	// リクエストの内容を取得
-	var req AddRequest
-	d := json.NewDecoder(r.Body)
-	if err := d.Decode(&req); err != nil {
-		httperror.BadRequest(w, err)
-		return
-	}
-
-	if len(req.Username) < 1 {
-		httperror.BadRequest(w, fmt.Errorf("empty username"))
-		return
-	}
-
-	// 同じユーザー名がいるかチェック
-	a, err := h.app.Dao.Account().FindByUsername(ctx, req.Username)
+	account, err := parseRequest(ctx, r, h.app.Dao.Account())
 	if err != nil {
-		httperror.InternalServerError(w, err)
-		return
-	} else if a != nil {
-		httperror.Error(w, http.StatusConflict)
-		return
-	}
-
-	account := new(object.Account)
-	account.Username = req.Username
-	if err = account.SetPassword(req.Password); err != nil {
-		httperror.InternalServerError(w, err)
-		return
+		switch err.(type) {
+		case *errBadrequest:
+			httperror.BadRequest(w, err)
+			return
+		case *errConflict:
+			httperror.Error(w, http.StatusConflict)
+			return
+		default:
+			httperror.InternalServerError(w, err)
+			return
+		}
 	}
 
 	// データベースにアカウント作成
